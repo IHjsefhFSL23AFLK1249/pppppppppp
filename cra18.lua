06330638164a292eeba9dd5aa4e1a7208c9fce62
IHjsefhFSL23AFLK1249 <149720967+IHjsefhFSL23AFLK1249@users.noreply.github.com>
2023-12-01 16:13:52 +0200

    Update and rename cra17.lua to cra18.lua

--- cra18.lua
+++ cra18.lua
@@ -7755,21 +7755,6 @@ if cmd == prefix.. "bypvgcrash2" then
 					end
 					end)		
 					cht("<ssuper gear me 000000000000000000000094794847")		
-					task.spawn(function()
-					while getgenv().trolering do
-						haiitv5()
-						task.spawn(function()
-						if wtf then
-							task.spawn(function()
-wtf = false
-cht("<ter")
-wait(.4)
-cht("<ter2")
-wtf = false
-wait(.4)
-wtf = true
-							end)
-						end
 end)
 repeat haiitv5() pcall(function() 
 	lp.Backpack:FindFirstChild("VampireVanquisher").Parent = lp.Character
